cf3bbd5c8e6febf890dcc4e2a42c8b07dae32853
Ben <slybenjamin@gmail.com>
2020-02-25 09:17:34 +1000

    Added WorkPhone andFax to the card

--- src/Components/Contacts/OfficeCard.js
+++ src/Components/Contacts/OfficeCard.js
@@ -3,12 +3,22 @@ import { withStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent'
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import { blue } from '@material-ui/core/colors';
 import MoreIcon from '@material-ui/icons/More';
 import config from '../../Config';
-import { Business } from '@material-ui/icons'
+import { 
+  Business,
+  Work,
+  Email,
+  Print, } from '@material-ui/icons'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Divider from '@material-ui/core/Divider';
 
 const classes = theme => ({
     expand: {
@@ -26,6 +36,10 @@ const classes = theme => ({
     },
   });
 
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
+}
+
 class OfficeCard extends Component {
     render() {
         return (
@@ -35,7 +49,32 @@ class OfficeCard extends Component {
                         avatar={<Business/>} 
                         title={this.props.office.Title}
                         subheader={this.props.office.EMail}/>
-
+                    <Divider/>
+                    <CardContent>
+                      <List>
+                        <ListItemLink href={"tel:" + this.props.office.WorkPhone}>
+                          <ListItemAvatar>
+                            <Avatar>
+                              <Work/>
+                            </Avatar>
+                          </ListItemAvatar>
+                          <ListItemText primary="Main Office Number" secondary={this.props.office.WorkPhone} />
+                        </ListItemLink>
+                        <Divider variant="inset" component="li"/>
+                        {(this.props.office.WorkFax !== "") && (
+                          <div>
+                            <ListItemLink href={"tel:" + this.props.office.WorkFax}>
+                              <ListItemAvatar>
+                                <Avatar>
+                                  <Print/>
+                                </Avatar>
+                              </ListItemAvatar>
+                              <ListItemText primary="Fax Number" secondary={this.props.office.WorkFax}/>
+                            </ListItemLink>
+                          </div>
+                        )}
+                      </List>
+                    </CardContent>
                 </Card>
             </div>
         )
